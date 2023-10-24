8a5031ce85bce730e870efea5ea4ab27d5c28879
topipot <148019240+topipot@users.noreply.github.com>
2023-10-24 17:40:06 +0800

    initial

--- main.py
+++ main.py
@@ -1,74 +1,47 @@
-from kivy.app import App 
+from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
+from kivy.uix.image import Image
+from kivy.uix.label import Label
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+import cv2
+import qrcode
+from pyzbar.pyzbar import decode
 
-
-class MainApp(App):
+class Wow(App):
     def build(self):
-        self.operators = ["/", "*", "+", "-"]
-        self.last_was_operator = None
-        self.last_button = None
+        self.layout = BoxLayout(orientation='vertical')
+        self.camera_image = Image()
+        self.layout.add_widget(self.camera_image)
+        self.results_label = Label(text="Scanning QR codes...")
+        self.layout.add_widget(self.results_label)
+        self.capture = cv2.VideoCapture(0)
+        Clock.schedule_interval(self.update, 1.0/30.0)  # Update every 30 frames per second
+        return self.layout
 
-        main_layout = BoxLayout(orientation = "vertical")
-        self.solution = TextInput(background_color = "black", foreground_color = "white",
-                                  multiline=False, halign= 'right', font_size=55, 
-                                  readonly=True)
+    def update(self, dt):
+        ret, frame = self.capture.read()
+        if ret:
+            decoded_objects = decode(frame)
+            self.show_frame(frame, decoded_objects)
 
-        main_layout.add_widget(self.solution)
-        buttons = [
-            ["7", "8","9", "/"],
-            ["4", "5","6", "*"],
-            ["1", "2","3", "+"],
-            [".", "0","C", "-"]
-        ]
+    def show_frame(self, frame, decoded_objects):
+        for obj in decoded_objects:
+            x, y, w, h = obj.rect
+            qr_data = obj.data.decode('utf-8')
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(frame, qr_data, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
 
-        for row in buttons:
-            h_layout = BoxLayout()
-            for label in row:
-                button = Button(
-                    text = label, font_size=30, background_color="grey",
-                    pos_hint={"center_x": 0.5, "center_y": 0.5},
-                )
-                button.bind(on_press=self.on_button_press)
-                h_layout.add_widget(button)
-            main_layout.add_widget(h_layout)
+        buf1 = cv2.flip(frame, 0)
+        buf = buf1.tostring()
+        image_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+        image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+        self.camera_image.texture = image_texture
 
-        equal_button = Button(
-            text= "=", font_size=30, background_color="grey",
-            pos_hint={"center_x": 0.5, "center_y": 0.5},
-        )
-
-        equal_button.bind(on_press=self.on_solution)
-        main_layout.add_widget(equal_button)
-
-        return main_layout
-    
-    def on_button_press (self, instance):
-        current = self.solution.text
-        button_text = instance.text
-
-        if button_text == 'C':
-            self.solution.text = ""
+        if decoded_objects:
+            self.results_label.text = "\n".join([obj.data.decode('utf-8') for obj in decoded_objects])
         else:
-            if current and (
-                self.last_was_operator and button_text in self.operators):
-                return 
-            elif current == "" and button_text in self.operators:
-                return
-            else:
-                new_text = current + button_text
-                self.solution.text =  new_text
+            self.results_label.text = "Scanning QR codes..."
 
-        self.last_button = button_text
-        self.last_was_operator = self.last_button in self.operators
-
-    def on_solution(self, instance):
-        text = self.solution.text   
-        if text:
-            solution = str(eval(self.solution.text))
-            self.solution.text = solution
-
-if __name__ == "__main__":
-    app = MainApp()
-    app.run()
+if __name__ == '__main__':
+    Wow().run()
