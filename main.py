dbf9a40eefc0a4f63f05e6b85df84b8b6e686b34
topipot <148019240+topipot@users.noreply.github.com>
2023-11-01 16:28:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.uix.image import Image
-from kivy.uix.label import Label
+from kivymd.uix.label import MDLabel
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 import cv2
 from kivy.utils import platform
 
 
-class Wow(App):
+class Wow(MDApp):  # Inherit from MDApp instead of App
     def build(self):
 
         if platform == 'android':
             from android.permissions import request_permissions, Permission
             request_permissions([Permission.CAMERA])
 
-        self.layout = BoxLayout(orientation='vertical')
+        self.layout = MDBoxLayout(orientation='vertical')  # Use MDBoxLayout instead of BoxLayout
         self.camera_image = Image()
         self.layout.add_widget(self.camera_image)
-        self.results_label = Label(text="Scanning QR codes...")
+
+        self.results_label = MDLabel(text="Scanning QR codes...", halign="center", valign="middle")  # Use MDLabel instead of Label
+        self.results_label.size_hint = (1, None)
         self.layout.add_widget(self.results_label)
+
         self.capture = cv2.VideoCapture(0)
-        Clock.schedule_interval(self.update, 1.0/30.0)  # Update every 30 frames per second
+        Clock.schedule_interval(self.update, 1.0 / 30.0)  # Update every 30 frames per second
 
         return self.layout
-            
 
     def update(self, dt):
         ret, frame = self.capture.read()
@@ -37,6 +39,7 @@ class Wow(App):
         image_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
         image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
         self.camera_image.texture = image_texture
+
 
 if __name__ == '__main__':
     Wow().run()
