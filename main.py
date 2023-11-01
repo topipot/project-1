d000c3cd90338291203902b8fffee1fef28ea8fe
topipot <148019240+topipot@users.noreply.github.com>
2023-11-01 16:02:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,20 +7,8 @@ from kivy.graphics.texture import Texture
 import cv2
 from kivy.utils import platform
 
-
 class Wow(App):
     def build(self):
-
-        if platform == 'android':
-            from android.permissions import request_permissions, Permission, check_permission
-            
-            if not check_permission(Permission.CAMERA):
-                self.request_camera_permission(None)
-                self.permission_button = Button(text="Request Camera Permission")
-                self.permission_button.bind(on_press=self.request_camera_permission)
-                self.layout.add_widget(self.permission_button)
-            else:
-                self.start_camera()
 
         self.layout = BoxLayout(orientation='vertical')
         self.camera_image = Image()
@@ -29,21 +17,15 @@ class Wow(App):
         self.layout.add_widget(self.results_label)
         self.capture = cv2.VideoCapture(0)
         Clock.schedule_interval(self.update, 1.0/30.0)  # Update every 30 frames per second
+
+        if platform == 'android':
+            from android.permissions import request_permissions, Permission
+            request_permissions([Permission.CAMERA])
         return self.layout
-    
-    def request_camera_permission(self, instance):
-        from android.permissions import request_permissions, Permission
-        request_permissions([Permission.CAMERA])
-        
-        # Check for permission and start the camera if granted
-        if check_permission(Permission.CAMERA):
-            self.layout.remove_widget(self.permission_button)
-            self.start_camera()
             
     def start_camera(self):
         self.capture = cv2.VideoCapture(0)
         Clock.schedule_interval(self.update, 1.0/30.0)
-    
 
     def update(self, dt):
         ret, frame = self.capture.read()
