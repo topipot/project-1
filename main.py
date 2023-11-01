6bf173d0e6608f8ba7659c91e6e57b081d4bf225
topipot <148019240+topipot@users.noreply.github.com>
2023-11-01 16:37:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,37 @@
 from kivymd.app import MDApp
-from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
-from kivymd.uix.label import MDLabel
 from kivy.clock import Clock
 from kivy.graphics.texture import Texture
 import cv2
 from kivy.utils import platform
+from kivymd.uix.button import MDRaisedButton
 
 
-class Wow(MDApp):  # Inherit from MDApp instead of App
+class Wow(MDApp):
     def build(self):
-
         if platform == 'android':
             from android.permissions import request_permissions, Permission
             request_permissions([Permission.CAMERA])
 
-        self.layout = MDBoxLayout(orientation='vertical')  # Use MDBoxLayout instead of BoxLayout
+        self.layout = BoxLayout(orientation='vertical')
+
+        # Add Start Camera Button
+        self.start_button = MDRaisedButton(text="Start Camera")
+        self.start_button.size_hint = (1, 0.2)
+        self.start_button.bind(on_press=self.start_camera)
+        self.layout.add_widget(self.start_button)
+
         self.camera_image = Image()
         self.layout.add_widget(self.camera_image)
 
-        self.results_label = MDLabel(text="Scanning QR codes...", halign="center", valign="middle")  # Use MDLabel instead of Label
-        self.results_label.size_hint = (1, None)
-        self.layout.add_widget(self.results_label)
-
-        self.capture = cv2.VideoCapture(0)
-        Clock.schedule_interval(self.update, 1.0 / 30.0)  # Update every 30 frames per second
 
         return self.layout
+
+    def start_camera(self, *args):
+        self.capture = cv2.VideoCapture(0)
+        Clock.schedule_interval(self.update, 1.0 / 30.0)
+        self.start_button.disabled = True  # Disable the button
 
     def update(self, dt):
         ret, frame = self.capture.read()
