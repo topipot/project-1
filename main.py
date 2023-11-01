906ca4359d2b3490313642e545d53bfd5b29de3a
topipot <148019240+topipot@users.noreply.github.com>
2023-11-01 20:44:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ import cv2
 from kivy.utils import platform
 from kivymd.uix.button import MDRaisedButton
 
-
 class Wow(MDApp):
     def build(self):
         if platform == 'android':
-            from android.permissions import request_permissions, Permission
-            request_permissions([Permission.CAMERA])
+            from android.permissions import request_permissions, Permission, check_permission
+
+            # Check if CAMERA permission is granted
+            if not check_permission(Permission.CAMERA):
+                request_permissions([Permission.CAMERA])
 
         self.layout = BoxLayout(orientation='vertical')
 
@@ -25,26 +27,36 @@ class Wow(MDApp):
         self.camera_image = Image()
         self.layout.add_widget(self.camera_image)
 
-
         return self.layout
 
     def start_camera(self, *args):
-        self.capture = cv2.VideoCapture(0)
-        Clock.schedule_interval(self.update, 1.0 / 30.0)
-        self.start_button.disabled = True  # Disable the button
+        try:
+            self.capture = cv2.VideoCapture(0)
+            if not self.capture.isOpened():
+                raise Exception("Camera not opened")
+
+            Clock.schedule_interval(self.update, 1.0 / 30.0)
+            self.start_button.disabled = True  # Disable the button
+        except Exception as e:
+            print(f"Error starting camera: {e}")
 
     def update(self, dt):
-        ret, frame = self.capture.read()
-        if ret:
-            self.show_frame(frame)
+        try:
+            ret, frame = self.capture.read()
+            if ret:
+                self.show_frame(frame)
+        except Exception as e:
+            print(f"Error updating frame: {e}")
 
     def show_frame(self, frame):
-        buf1 = cv2.flip(frame, 0)
-        buf = buf1.tostring()
-        image_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
-        image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
-        self.camera_image.texture = image_texture
-
+        try:
+            buf1 = cv2.flip(frame, 0)
+            buf = buf1.tobytes()
+            image_texture = Texture.create(size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+            image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+            self.camera_image.texture = image_texture
+        except Exception as e:
+            print(f"Error displaying frame: {e}")
 
 if __name__ == '__main__':
     Wow().run()
